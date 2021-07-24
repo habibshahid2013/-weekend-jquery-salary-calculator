895d4be4eda7bfcb1b3d26648e9e6c893ce6c36d
Hassan Shahid <habibshahid2013@gmail.com>
2021-07-24 13:28:33 -0500

    added css and added information to the DOM

--- client.js
+++ client.js
@@ -1,13 +1,18 @@
 console.log('testing JS 😀');
 
+let allEmployees = []
+
 $(document).ready(onReady);
 function onReady(){
     console.log('so ready 🇺🇸');
 //button to submit information on form
     $('#submitBtn').on('click', onSubmit)
 
-//when we submit the product form
-    $('#productForm').on('submit', onSubmit)
+//when we submit the employee form
+    $('#employeeForm').on('submit', onSubmit)
+
+//button to delete information
+    $('#deleteBtn').on('click', deleteFunction)
 }
 
 function onSubmit(event){
@@ -18,30 +23,59 @@ function onSubmit(event){
     console.log('onSubmit');
 
     //grab values from form
-    let product = {
-    name: $('#nameInput').val(),
-    color: $('#colorInput').val(),
-    size: $('#sizeInput').val(),
-    price:  Number($('#priceInput').val()),
+    let employee = {
+    firstName: $('#firstNameInput').val(),
+    lastName: $('#lastNameInput').val(),
+    jobID: $('#idInput').val(),
+    jobTitle: $('#jobInput').val(),
+    annualSalary:  Number($('#salaryInput').val()),
     };
+    allEmployees.push(employee)
 
-    console.log('product', product);
+    console.log('all of the employees', allEmployees);
 
     //focus on name input
-    $('nameInput').focus();
+    $('firstNameInput').focus();
 
     //Clear form Input
-    $('#nameInput').val('')
-    $('#colorInput').val('')
-    $('#priceInput').val('')
+    $('#firstNameInput').val('')
+    $('#lastNameInput').val('')
+    $('#salaryInput').val('')
+    $('#idInput').val('')
+    $('#salaryInput').val('')
+    
 
-    //clear form input
-    $('#productTable').append(` 
+    //rendering of employee
+    $('#employeeTable').append(` 
          <tr>
-            <td>${product.name}</td>
-            <td>${product.color}</td>
-            <td>${product.size}</td>
-            <td>${product.price}</td>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.jobID}</td>
+            <td>${employee.jobTitle}</td>
+            <td>$${employee.annualSalary}</td>
+            <td><button>Delete</button></td>
+        </tr>`
+    );
+    //calculate Monthly Cost
+    let monthlyCost = 0 
+    for (let employee of allEmployees){
+        monthlyCost += employee.annualSalary;
+     if (monthlyCost > 20000){
+         $('#totalSalry').css('background-color', 'red');
+     }
+    }
+
+    $('#totalSalary').text(employee.annualSalary.toFixed(2));
+   
+    };
+
+function deleteFunction() {
+    $('#employeeTable').empty(` 
+         <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.jobID}</td>
+            <td>${employee.jobTitle}</td>
         </tr>`
     )
 
